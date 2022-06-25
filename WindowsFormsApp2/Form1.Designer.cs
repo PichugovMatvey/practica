26a0a7ec7ab17a3d80ad6fc32dfadf12c60c51a5
PichugovMatvey <108180896+PichugovMatvey@users.noreply.github.com>
2022-06-25 15:29:16 +0300

    Add files via upload

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(402, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(382, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 60;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(310, 110);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(342, 265);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 312);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] mass = new int[10];
+        Random rand = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            dataGridView1.ColumnCount = 10;
+            dataGridView1.RowCount = 4;
+            dataGridView1.Rows[0].HeaderCell.Value = "x";
+            dataGridView1.Rows[1].HeaderCell.Value = "y";
+            dataGridView1.Rows[2].HeaderCell.Value = "xy";
+            dataGridView1.Rows[3].HeaderCell.Value = "xx";
+
+            for (int i = 0; i < 10; i++)
+            {
+                dataGridView1.Columns[i].Width = 25;
+                dataGridView1.Rows[0].Cells[i].Value = 0;
+                
+                dataGridView1.Rows[1].Cells[i].Value = 0;
+                
+                dataGridView1.Rows[2].Cells[i].Value = 0;
+                
+                dataGridView1.Rows[3].Cells[i].Value = 0;
+            }
+            chart1.Series.Add("pmiseries");
+            chart1.Series.Add("линия");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                mass[i] = rand.Next(-20, 80);
+
+            }
+            
+            
+            chart1.Series["pmiseries"].Points.Clear();
+            chart1.Series["линия"].Points.Clear();
+
+            int sumx = 0;
+            int sumy = 0;
+            int sumxy = 0;
+            int sumxx = 0;
+            chart1.Series["pmiseries"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series["линия"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            for (int i = 0; i < 10; i++)
+            {
+                dataGridView1.Columns[i].HeaderText = Convert.ToString(i+1);
+                sumx += i;
+                dataGridView1.Rows[0].Cells[i].Value = i ;
+                sumy += mass[i];
+                dataGridView1.Rows[1].Cells[i].Value = mass[i] ;
+                sumxy += i * mass[i];
+                dataGridView1.Rows[2].Cells[i].Value = i*mass[i];
+                sumxx += i * i;
+                dataGridView1.Rows[3].Cells[i].Value = i*i;
+                
+            }
+            double a = (10 * sumxy - sumx * sumy) / (10 * sumxx - sumx * sumx);
+            double b = (sumy - sumx * a) / 10;
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                
+                chart1.Series["pmiseries"].Points.Add(mass[i]);
+                double  fx = a * i + b;
+                chart1.Series["линия"].Points.Add(fx);
+            }
+
+        }
+        
+    
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
